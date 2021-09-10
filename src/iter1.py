90e62b69330104ced9f83ad6dda4b16892994ef2
Fayoumi98 <omarselfayoumi@gmail.com>
2021-09-10 13:50:54 +0200

    mpc on racing line

--- src/iter1.py
+++ src/iter1.py
@@ -6,6 +6,16 @@ import sys
 import csv
 import pandas as pd
 
+
+
+
+
+
+
+
+
+
+
 # Vehicle parameters
 LENGTH = 4.5  # [m]
 WIDTH = 2.0  # [m]
@@ -14,6 +24,29 @@ WHEEL_LEN = 0.3  # [m]
 WHEEL_WIDTH = 0.2  # [m]
 TREAD = 0.7  # [m]
 WB = 2.5  # [m]
+TARGET_SPEED = 10.0 / 3.6  # [m/s] target speed
+
+
+
+
+
+
+
+
+class State:
+    """
+    vehicle state class
+    """
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.predelta = None
+
+
+
 
 
 
@@ -79,18 +112,127 @@ def plot_car(x, y, yaw, steer=0.0, cabcolor="-r", truckcolor="-k"):  # pragma: n
 
 
 
+
+
+
+
+def get_yaw(cx,cy):
+    
+    idx = []
+    yaw = []
+    yaw.append(0)
+    idx.append(0)
+
+    for i in range(len(cx)-1):
+        dx = cx[i+1]-cx[i]
+        dy = cy[i+1]-cy[i]
+        yaw_calc = math.atan2(dy, dx)
+
+        yaw.append(yaw_calc)
+        idx.append(i+1)
+
+    #print(len(cx),len(cy),len(yaw))
+    #print(cx,cy,yaw)
+    #print(idx)
+
+    return yaw , idx
+
+
+
+
+
+
+
+
+
+
 def create_track():
     cx = []
     cy = []
     
-    data = pd.read_csv('/home/fayo/model_predictive_control/src/racetrack-database/tracks/handling_track.csv')
+    data = pd.read_csv('/home/fayo/grad_proj/mpc_project/src/racetrack-database/tracks/Silverstone.csv')
     
     for i, j in data.iterrows():
-        cx.append(j[0])
-        cy.append(j[1])
+        cx.append(j[0]/30)
+        cy.append(j[1]/30)
+    
+    yaw , idx = get_yaw(cx,cy)
+    
 
     plt.scatter(cx,cy)
+
+    return cx , cy , yaw , idx
     
+
+
+
+
+
+
+
+
+
+def pi_2_pi(angle):
+    while(angle > math.pi):
+        angle = angle - 2.0 * math.pi
+
+    while(angle < -math.pi):
+        angle = angle + 2.0 * math.pi
+
+    return angle
+
+
+
+
+
+
+
+
+
+
+def calc_speed_profile(cx, cy, cyaw, target_speed):
+
+    speed_profile = [target_speed] * len(cx)
+    direction = 1.0  # forward
+
+    # Set stop point
+    for i in range(len(cx) - 1):
+        dx = cx[i + 1] - cx[i]
+        dy = cy[i + 1] - cy[i]
+
+        move_direction = math.atan2(dy, dx)
+
+        if dx != 0.0 and dy != 0.0:
+            dangle = abs(pi_2_pi(move_direction - cyaw[i]))
+            if dangle >= math.pi / 4.0:
+                direction = -1.0
+            else:
+                direction = 1.0
+
+        if direction != 1.0:
+            speed_profile[i] = - target_speed
+        else:
+            speed_profile[i] = target_speed
+
+    speed_profile[-1] = 0.0
+
+    return speed_profile
+
+
+
+
+
+def do_simulation(cx, cy, cyaw, ck, sp, dl, initial_state):
+    """
+    Simulation
+    cx: course x position list
+    cy: course y position list
+    cy: course yaw position list
+    ck: course curvature list
+    sp: speed profile
+    dl: course tick [m]
+    """
+    pass
 
 
 
@@ -99,12 +241,17 @@ def create_track():
 def main():
     
     while True:
-        i = 0
-        create_track()
+
+        cx , cy , cyaw , idx = create_track()
         plot_car(0,0,0)
+
+        sp = calc_speed_profile(cx, cy, cyaw, TARGET_SPEED)
+        
+        initial_state = State(x=cx[0], y=cy[0], yaw=cyaw[0], v=0.0)
+
         plt.pause(0.1)
-        #plt.show()
-        i+=1
+
+
     
 
 
